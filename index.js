9108a28e700d86941d760e332b3fd81983a1e180
Arkadiusz Kulpa <arkadiusz.kulpa.91@gmail.com>
2021-09-14 11:01:28 +0100

    diceroll perfected

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var dice1string = "images/dice" + randomNumber + ".png";
-document.getElementById('img1').setAttribute("src", dice1string);
 
-var dice2string = "images/dice" + randomNumber2 + ".png";
-document.getElementById("img2").setAttribute("src", dice2string);
+function throwDice() {
 
-function setWinnerTitle(randomNumber, randomNumber2) {
+  var randomNumber = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  var dice1string = "images/dice" + randomNumber + ".png";
+  document.getElementById('img1').setAttribute("src", dice1string);
+
+  var dice2string = "images/dice" + randomNumber2 + ".png";
+  document.getElementById("img2").setAttribute("src", dice2string);
+
   if (randomNumber > randomNumber2) {
-    document.getElementById("Title").innerHTML = "Player 1 Wins!";
-  } else {
-    document.getElementById("Title").innerHTML = "Player 2 Wins!";
+    document.getElementById("Title").innerHTML = "🚩 Player 1 Wins!";
+  } else if (randomNumber < randomNumber2) {
+    document.getElementById("Title").innerHTML = "Player 2 Wins! 🚩";
+  } else if (randomNumber == randomNumber2) {
+    document.getElementById("Title").innerHTML = "Draw!";
   }
 }
 
-setWinnerTitle(randomNumber, randomNumber2);
+const button = document.querySelector('button');
+
+// button.addEventListener('click', event => {
+//   button.textContent = `Click count: ${event.detail}`;
+// });
+
+button.addEventListener('click', event => {
+  throwDice();
+})
